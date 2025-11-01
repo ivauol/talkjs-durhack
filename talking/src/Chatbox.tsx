cb2f869b47bc0d9a0f0493966020e7f56c1f55f2
ivauol <fwpn2259@leeds.ac.uk>
2025-11-01 18:45:26 +0000

    Setup of starter code

---
+++ talking/src/Chatbox.tsx
@@ -0,0 +1,37 @@
+import { Chatbox } from '@talkjs/react-components';
+import '@talkjs/react-components/default.css';
+import { getTalkSession } from '@talkjs/core';
+import { useEffect } from 'react';
+
+export default function Chat() {
+  const appId = 'tHMayYki';
+  const userId = 'frank';
+  const otherUserId = 'nina';
+  const conversationId = 'new_conversation';
+  const session = getTalkSession({
+    // @ts-ignore
+    host: 'durhack.talkjs.com',
+    appId,
+    userId,
+  });
+
+  useEffect(() => {
+    session.currentUser.createIfNotExists({ name: 'Frank' });
+    session.user(otherUserId).createIfNotExists({ name: 'Nina' });
+
+    const conversation = session.conversation(conversationId);
+    conversation.createIfNotExists();
+    conversation.participant(otherUserId).createIfNotExists();
+  }, [session, conversationId, otherUserId]);
+
+  return (
+    <Chatbox
+      // @ts-ignore
+      host="durhack.talkjs.com"
+      style={{ width: '400px', height: '600px' }}
+      appId={appId}
+      userId={userId}
+      conversationId={conversationId}
+    />
+  );
+}
